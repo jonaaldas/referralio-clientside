ad449fbbd8753db969358d737303e386d3372323
Jonathan Aldas <jonaaldas@gmail.com>
2022-09-21 09:14:35 -0500

    added a slash to the url

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 // user apis
 
 // const url = 'http://localhost:5015/api/user/'
-const url = 'https://referral-io.adaptable.app/api/user'
+const url = 'https://referral-io.adaptable.app/api/user/'
 
 export const registerUserRequest = async (name, email, password) => {
   try {
