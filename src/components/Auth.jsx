a97291ee236a5b90d716c7b5ccef07b98caf9361
Jonathan Aldas <jonaaldas@gmail.com>
2022-09-24 16:20:19 -0500

    fixed something from register page

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -58,7 +58,7 @@ function Auth() {
 			}}
 		>
 			{({ handleSubmit, handleChange }) => (
-				<div className='w-full min-h-screen bg-gray-50 flex flex-col sm:justify-center items-center pt-6 sm:pt-0'>
+				<div className='w-full min-h-screen bg-gray-50 flex flex-col  items-center pt-6 sm:pt-0'>
 					<div className='w-full sm:max-w-md p-5 mx-auto'>
 						<h2 className='mb-12 text-center text-5xl font-extrabold'>
 							{alreadyHaveAccount === false ? "Register" : "Log In"}
